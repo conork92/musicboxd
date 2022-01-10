f41829cab3ffd2d8b4ad4591474c96b20b075524
Conor Kennedy <conorkennedy999@gmail.com>
2022-01-10 23:54:34 +0800

    addition of a s3 bucket

---
+++ terraform/s3.tf
@@ -0,0 +1,21 @@
+resource "aws_s3_bucket" "host" {
+  bucket = "s3-website-test.hashicorp.com"
+  acl    = "public-read"
+  policy = file("policies/s3-policy.json")
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+
+    routing_rules = <<EOF
+[{
+    "Condition": {
+        "KeyPrefixEquals": "docs/"
+    },
+    "Redirect": {
+        "ReplaceKeyPrefixWith": "documents/"
+    }
+}]
+EOF
+  }
+}
