8f7204abb9b3cd6ec2cc3e23f1355f71a760d8e9
Conor Kennedy <conorkennedy999@gmail.com>
2022-01-13 20:26:13 +0800

    added linux instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,3 +19,12 @@ resource "aws_budgets_budget" "musicboxd_budget" {
   time_unit         = "MONTHLY"
   time_period_start = "2022-01-01_00:01"
 } 
+
+resource "aws_instance" "linux_server" {
+  ami           = "ami-096f43ef67d75e998"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Musicboxd Server"
+  }
+}
