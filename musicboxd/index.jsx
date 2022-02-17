f37c79cc496d2636f628b8c286c4733e72e1d101
Conor Kennedy <conorkennedy999@gmail.com>
2022-02-17 20:39:16 +0800

    added more to the index.jsx

--- musicboxd/index.jsx
+++ musicboxd/index.jsx
@@ -19,6 +19,22 @@ var app = (
 )
 
 
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        const welcome = this.props.name;
+        return (
+            
+            <div>
+                <h1>Hello, {this.props.name}</h1>
+                <p>Welcome to your first React Class Component!</p>
+            </div>
+        );
+    }
+}
+
 
 ReactDOM.render(app,
     document.getElementById("root"))
